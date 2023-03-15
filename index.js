514c3f3845b624a676d02ff871f28a3178e3b424
Eva Halfarova <halfarovaevacz@gmail.com>
2023-03-15 20:33:47 +0100

    verze 1

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log('funguju!');
+
+const timeIsUp = () => {
+const alarm = document.querySelector(".alarm")
+alarm.classList.add("alarm--ring")
+}
+
+setTimeout(timeIsUp, 5000)
+
+const userQuestion = prompt("Kdy má začít zvonit minutka?")
+
+const zvoneni = document.querySelector(".zvukMinutky")
+
+function spustAudio(){
+    zvoneni.play()
+}
+/*
+const stopRinging= ()=>
+{
+    const button = document.querySelector (".btn_stop")
+    button.classList.add("alarm")
+}
+
+document.addEventListener("click", stopRinging)
+*/
